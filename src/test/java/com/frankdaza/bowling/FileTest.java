46f0c9dc9c145e3c1784100160be732e16759407
Frank Edward Daza González <frank.daza2@gmail.com>
2019-08-01 15:25:04 -0500

    Created read file test.

---
+++ src/test/java/com/frankdaza/bowling/FileTest.java
@@ -0,0 +1,41 @@
+/**
+ * @author Frank Edward Daza González
+ * @date 2019-08-01
+ */
+package com.frankdaza.bowling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Frank Edward Daza González
+ * @date 2019-08-01
+ */
+public class FileTest {
+	
+	private static final Logger log = Logger.getLogger(FileTest.class);
+	
+	/**
+	 * Test functionality of reading a file.
+	 * 
+	 * @author Frank Edward Daza González
+	 * @date 2019-08-01
+	 */
+	@Test
+	public void readFileTest() {
+		try (Stream<String> stream = Files.lines(Paths.get("src/test/resources/fileTest.txt"))) {
+
+			stream.forEach(System.out::println);
+			stream.close();
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+		}
+		
+	}
+
+}
