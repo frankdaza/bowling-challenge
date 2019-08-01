94af429ca3854509262b87dbe87d9a7bc7e2119a
Frank Edward Daza González <frank.daza2@gmail.com>
2019-08-01 14:54:26 -0500

    Created main Bowling class. Added maven-jar-plugin to pom.

---
+++ src/main/java/com/frankdaza/bowling/main/Bowling.java
@@ -0,0 +1,22 @@
+/**
+ * @author Frank Edward Daza González
+ * @date 2019-08-01
+ */
+package com.frankdaza.bowling.main;
+
+/**
+ * @author Frank Edward Daza González
+ * @date 2019-08-01
+ */
+public class Bowling {
+
+	/**
+	 * @author Frank Edward Daza González
+	 * @date 2019-08-01
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
