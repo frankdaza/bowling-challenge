f5abbda872cfdbc8505d91f9e9ed9dd51cb86035
Frank Edward Daza González <frank.daza2@gmail.com>
2019-08-02 11:48:50 -0500

    Created functionality that separete each player with its score, into a linkedHashMap.

--- src/main/java/com/frankdaza/bowling/service/ScoreService.java
+++ src/main/java/com/frankdaza/bowling/service/ScoreService.java
@@ -4,13 +4,22 @@
  */
 package com.frankdaza.bowling.service;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.WordUtils;
+
+import com.frankdaza.bowling.domain.Player;
 
 /**
  * @author Frank Edward Daza González
  * @date 2019-08-02
  */
 public class ScoreService {
+	
+	private LinkedHashMap<String, List<Integer>> playersLinkedHashMap = new LinkedHashMap<>();
 	
 	
 	/**
@@ -21,7 +30,47 @@ public class ScoreService {
 	 * @param lines
 	 */
 	public void convertFileLinesToScores(List<String> lines) {
-		System.out.println("Convert file lines to scores!");
+		for (String line : lines) {
+			String[] lineArray = line.split("\t");
+			String name = WordUtils.capitalize(lineArray[0].trim());
+			
+			Integer score = getScoreFromString(lineArray[1]);
+			
+			
+			if (this.playersLinkedHashMap.get(name) != null) {
+				List<Integer> actualScore = this.playersLinkedHashMap.get(name);
+				actualScore.add(score);
+				
+				this.playersLinkedHashMap.put(name, actualScore);
+			} else {
+				List<Integer> actualScore = new ArrayList<Integer>();
+				actualScore.add(score);
+				this.playersLinkedHashMap.put(name, actualScore);
+			}
+			
+		}
+		
+		System.out.println(playersLinkedHashMap);
 	}
+	
+	/**
+	 * Auxiliar method that get the score from a line file score.
+	 * 
+	 * @author Frank Edward Daza González
+	 * @date 2019-08-02
+	 * @param lineScore
+	 * @return Integer Return player score.
+	 */
+	private Integer getScoreFromString(String lineScore) {
+		String lineScoreOld = lineScore.trim().toUpperCase();
+		
+		if (lineScoreOld.equals("F")) {
+			return 0;
+		} else {
+			return Integer.parseInt(lineScoreOld);
+		}
+	}
+	
+	
 
 }
